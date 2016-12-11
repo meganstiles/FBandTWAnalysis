cb82238854607ff5d381a24a9b61685af2ba61c9
Megan Stiles <megan.e.stiles@gmail.com>
2016-12-11 18:13:47 -0500

    fixed typos

--- Social_Analysis.py
+++ Social_Analysis.py
@@ -5,10 +5,10 @@ import numpy as np
 from string import punctuation
 from collections import Counter
 
-os.chdir('/Users/meganstiles/Desktop/Bowen/December 2016/')
-file1 = pd.read_csv('12-4 Metrics.csv', encoding = 'latin-1')
-file2 = pd.read_csv('12-4 Engagements.csv', encoding= 'latin-1')   
-file3 = pd.read_csv('12-4 Links.csv', encoding= 'latin-1') 
+#os.chdir('/Users/meganstiles/Desktop/Bowen/December 2016/')
+#file1 = pd.read_csv('12-4 Metrics.csv', encoding = 'latin-1')
+#file2 = pd.read_csv('12-4 Engagements.csv', encoding= 'latin-1')   
+#file3 = pd.read_csv('12-4 Links.csv', encoding= 'latin-1') 
 def get_analysis(file1,file2,file3):
     #Read in File 1 
     file1 = file1.drop([0])
@@ -30,15 +30,18 @@ def get_analysis(file1,file2,file3):
 
     #Comments
     Comments = file2['comment'].sum()
-    print('Comments:' + Comments)
+    print('Comments:')
+    print(Comments)
 
     #shares
     Shares = file2['share'].sum()
-    print('Shares:' + Shares)
+    print('Shares:')
+    print(Shares)
 
     #Links
     Links = file3['link clicks'].sum()
-    print('Links:' + Links)
+    print('Links:')
+    print(Links)
     
     hashtags = []
     for text in range(1,len(file1)):
@@ -50,16 +53,17 @@ def get_analysis(file1,file2,file3):
     unique_hashtags =np.unique(hashtags)
     unique_hashtags = sorted(unique_hashtags, key= str.lower)
     print('The hashtags used were ' + str(unique_hashtags))
-    print('The count of hashtags was:' + Counter(hashtags))
+    print('The count of hashtags was:')
+    print(Counter(hashtags))
     str1 = ''.join(str(e) for e in unique_hashtags)
     print(str1)
     
-get_analysis(file1,file2,file3)
+#get_analysis(file1,file2,file3)
 
 #Twitter
 
-os.chdir('/Users/meganstiles/Desktop/Bowen/December 2016/')
-file1 = pd.read_csv('tweet_activity_metrics_asdmra_20161128_20161205_en.csv', encoding = 'latin-1')
+#os.chdir('/Users/meganstiles/Desktop/Bowen/December 2016/')
+#file1 = pd.read_csv('tweet_activity_metrics_asdmra_20161128_20161205_en.csv', encoding = 'latin-1')
 
 def get_Tweets(file1):
     #Read in File 1 
@@ -77,15 +81,18 @@ def get_Tweets(file1):
 
     #Replies
     Replies = file1['replies'].sum()
-    print('Replies:' + Replies)
+    print('Replies:')
+    print(Replies)
 
     #Retweest
     Retweets = file1['retweets'].sum()
-    print('Retweets:' + Retweets)
+    print('Retweets:')
+    print(Retweets)
 
     #Links
     Links = file1['url clicks'].sum()
-    print('Links:' + Links)
+    print('Links:')
+    print(Links)
     
     hashtags = []
     for text in range(1,len(file1)):
@@ -104,41 +111,41 @@ get_Tweets(file1)
 
 
 #Just Hashtags for Facebook
-os.chdir('/Users/meganstiles/Desktop/Bowen/November 2016/')
-file1 = pd.read_csv('November Metrics.csv', encoding = 'latin-1')
+#os.chdir('/Users/meganstiles/Desktop/Bowen/November 2016/')
+#file1 = pd.read_csv('November Metrics.csv', encoding = 'latin-1')
 
-def get_FBHashtags(file1)
-file1 = file1.drop([0])
-hashtags = []
-for text in range(1,len(file1)):
-    post = file1['Post Message'][text]
-    post = re.sub("[:!.,]", '', post)
-    hashes = re.findall('#\S*', post)
-    for tag in hashes:
-        hashtags.append(tag)
-unique_hashtags =np.unique(hashtags)
-unique_hashtags = sorted(unique_hashtags, key= str.lower)
-print('The hashtags used were ' + str(unique_hashtags))
-print(Counter(hashtags))
-str1 = ''.join(str(e) for e in unique_hashtags)
-print(str1)
-file1[1]
+def get_FBHashtags(file1):
+    file1 = file1.drop([0])
+    hashtags = []
+    for text in range(1,len(file1)):
+        post = file1['Post Message'][text]
+        post = re.sub("[:!.,]", '', post)
+        hashes = re.findall('#\S*', post)
+        for tag in hashes:
+            hashtags.append(tag)
+    unique_hashtags =np.unique(hashtags)
+    unique_hashtags = sorted(unique_hashtags, key= str.lower)
+    print('The hashtags used were ' + str(unique_hashtags))
+    print(Counter(hashtags))
+    str1 = ''.join(str(e) for e in unique_hashtags)
+    print(str1)
+    file1[1]
 
 #Hashtags for Twitter
-os.chdir('/Users/meganstiles/Downloads/')
-file1 = pd.read_csv('tweet_activity_metrics_asdmra_20161101_20161201_en.csv', encoding = 'latin-1')
+#os.chdir('/Users/meganstiles/Downloads/')
+#file1 = pd.read_csv('tweet_activity_metrics_asdmra_20161101_20161201_en.csv', encoding = 'latin-1')
 
 def get_TWHashtags(file1):
-hashtags = []
-for text in range(1,len(file1)):
-    post = file1['Tweet text'][text]
-    post = re.sub("[:!.,]", '', post)
-    hashes = re.findall('#\S*', post)
-    for tag in hashes:
-        hashtags.append(tag)
-unique_hashtags =np.unique(hashtags)
-unique_hashtags = sorted(unique_hashtags, key= str.lower)
-print('The hashtags used were ' + str(unique_hashtags))
-print(Counter(hashtags))
-str1 = ''.join(str(e) for e in unique_hashtags)
-print(str1)
+    hashtags = []
+    for text in range(1,len(file1)):
+        post = file1['Tweet text'][text]
+        post = re.sub("[:!.,]", '', post)
+        hashes = re.findall('#\S*', post)
+        for tag in hashes:
+            hashtags.append(tag)
+    unique_hashtags =np.unique(hashtags)
+    unique_hashtags = sorted(unique_hashtags, key= str.lower)
+    print('The hashtags used were ' + str(unique_hashtags))
+    print(Counter(hashtags))
+    str1 = ''.join(str(e) for e in unique_hashtags)
+    print(str1)
