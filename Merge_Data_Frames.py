4c662d4118e63b1353aae9ee64a3416828eaa370
Megan Stiles <megan.e.stiles@gmail.com>
2016-12-26 17:31:50 -0500

    fixed merge and perform files

--- Merge_Data_Frames.py
+++ Merge_Data_Frames.py
@@ -10,27 +10,28 @@ import pandas as pd
 import numpy as np
 from string import punctuation
 from collections import Counter
-def merge_fb_files(file1,file2,file3):
+
+def merge_fb_files(Posts,Engagements,Links):
     
     #Drop top line (contains information we don't need)
-    file1_clean = file1.drop([0])
+    Posts_clean = Posts.drop([0])
     
     #Convert files to numeric   
-    file1_clean_2 = file1_clean.convert_objects(convert_numeric=True)
-    file2_clean = file2.convert_objects(convert_numeric=True)
-    file3_clean = file3.convert_objects(convert_numeric=True)
+    Posts_clean_2 = Posts_clean.convert_objects(convert_numeric=True)
+    Engagements_clean = Engagements.convert_objects(convert_numeric=True)
+    Links_clean = Links.convert_objects(convert_numeric=True)
 
     #Subset files with columns I need
-    Total = file1_clean2[[0,1,2,3, 6, 9]]
-    file2_clean_2 = file2_clean[[1,9,10,11]]
-    file3_clean_2 = file3_clean[[1,9]]
+    Total = Posts_clean_2[[0,1,2,3, 6, 9]]
+    Engagements_clean_2 = Engagements_clean[[1,9,10,11]]
+    Links_clean_2 = Links_clean[[1,9]]
 
     #Concatinate columns into One Data Frame
-    Data = pd.concat([Total, file3_clean_2,file2_clean_2], axis =1, join= 'inner')
+    Data = pd.concat([Total, Links_clean_2,Engagements_clean_2], axis =1, join= 'inner')
     Data = Data[[1,2,3,4,5,7,9,10,11]]
 
     #Write out CSV
-    Data.to_csv('12-18Total.csv')
+    Data.to_csv('FB_Clean.csv')
 
 #Twitter = pd.read_csv('tweet_activity_metrics_asdmra_20161212_20161219_en.csv')    
 def merge_Twitter_files(file):
@@ -40,5 +41,5 @@ def merge_Twitter_files(file):
     Twitter_new = file[[1,2,4,7,8,9,11]]
     
     #Write out to csv
-    Twitter_new.to_csv('Tweets.csv')
+    Twitter_new.to_csv('Tweets_clean.csv')
 
